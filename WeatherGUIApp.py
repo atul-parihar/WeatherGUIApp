ec379af8457b648e1e9e013c30db4cbf0df43c8f
atul-parihar <52665213+atul-parihar@users.noreply.github.com>
2020-03-10 20:26:24 +0530

    Add files via upload

---
+++ WeatherGUIApp.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+from tkinter import font
+import requests
+
+HEIGHT = 500
+WIDTH = 600
+
+def format_response(weather):
+    #print(weather)
+    try:
+        name = weather['name']
+        desc = weather['weather'][0]['description']
+        temp = weather['main']['temp']
+
+        final_str = 'City: %s \nConditions: %s \nTemperature (F): %s' % (name, desc, temp)       
+    except:
+        final_str = 'There was a problem retrieving that information'
+
+    return final_str 
+
+def get_weather(city):
+    weather_key = '53bd87018e4443ceb164947d3c74af1e'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID':weather_key, 'q':city, 'units': 'imperial'}
+    response = requests.get(url, params=params)
+    weather = response.json()
+
+    label['text'] = format_response(weather)
+
+root = tk.Tk()
+
+root.title("Weather App")
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+background_image = tk.PhotoImage(file='scenery.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='blue', bd=6)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry = tk.Entry(frame, font=("Courier", 18))
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text="Get Weather", font=("Courier", 12), command=lambda: get_weather(entry.get()))
+button.place(relx=0.7, relwidth=0.3, relheight=1)
+
+lower_frame = tk.Frame(root, bg='blue', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame, font=("Courier", 18), anchor='nw', justify='left', bd=4)
+label.place(relwidth=1, relheight=1)
+
+root.wm_iconbitmap('sun_icon.ico')
+root.mainloop() 
